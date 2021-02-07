43963fffa452e894b96f9d0dbbaa957006a29492
matt22881 <matt22881@gmail.com>
2021-02-07 07:06:26 -0500

    reset smurf display

--- client/src/components/SmurfDisplay.js
+++ client/src/components/SmurfDisplay.js
@@ -1,14 +1,6 @@
-            //Task List:
-            //1. Import in all needed components and library methods.
 import React from 'react';
-import { connect } from 'react-redux';
-import { getSmurfs } from '../actions';
-import { initialState } from './../reducers'
 
 export class SmurfDisplay extends React.Component {
-    // state = props.state
-    // console.log(props)
-
     render() {
         return(<div>
             
@@ -16,13 +8,11 @@ export class SmurfDisplay extends React.Component {
     }
 }
 
+export default SmurfDisplay;
+
+//Task List:
+//1. Import in all needed components and library methods.
 //2. Connect all needed redux state props and action functions to the component before exporting.
-const mapStateToProps = state => {
-    return {smurfs: state.smurfs};
-  };
-
-export default connect(mapStateToProps, { getSmurfs })(SmurfDisplay);
-
 //3. Fetch all smurfs when the component first mounts.
 //4. Render loading text or graphic if the application is currently loading.
 //5. Render a list of all Smurfs using the Smurf component if the application is not currently loading.
